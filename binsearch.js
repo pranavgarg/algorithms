308b542e42117cde5f44d6b91fa2571e2051a0ab
Joe Chasinga <jo.chasinga@gmail.com>
2017-02-04 00:58:26 -0500

    Switch to template literals

--- binsearch.js
+++ binsearch.js
@@ -9,24 +9,24 @@ function search(dst, arr) {
 
   arr.sort((a, b) => a-b);
 
-  console.log("Finding", dst, "in", arr);
+  console.log(`Finding ${dst} in`, arr);
 
   if ((arr.length == 1) && (arr[0] !== dst))
     return false;
 
   var middlebound = Math.floor(arr.length / 2);
 
-  if (dst == arr[middlebound]) {
+  if (dst == arr[middlebound])
     return true;
 
   if (dst < arr[middlebound]) {
-    console.log("%d is to the left...", dst);
+    console.log(`${dst} is to the left...`);
     let lowerbound = 0;
     let upperbound = middlebound;
     return search(dst, arr.slice(lowerbound, upperbound));
   } 
   else if (dst > arr[middlebound-1]) {
-    console.log("%d is to the right...", dst);
+    console.log(`${dst} is to the right...`);
     let lowerbound = middlebound;
     let upperbound = arr.length;
     return search(dst, arr.slice(lowerbound, upperbound));
