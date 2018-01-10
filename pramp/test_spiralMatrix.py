37f28b4debbc82e015ad8b5d8337a076509e9e23
Pranav Garg <pgarg@grovo.com>
2018-01-10 12:53:42 -0800

    updated circle.ci to run actual nosetests

---
+++ pramp/test_spiralMatrix.py
@@ -0,0 +1,8 @@
+from spiralMatrix import spiral_copy
+
+def test_spiral_copy():
+    assert spiral_copy([[1,2],[3,4]]) == [1,2,4,3]
+
+
+def test_spiral_copy():
+    assert spiral_copy([[1,2],[3,4]]) == [1,2,4,3]
