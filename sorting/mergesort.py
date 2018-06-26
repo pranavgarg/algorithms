42310b5a8e9a424d005f411e910a7e036d4b3988
Joe Chasinga <jo.chasinga@gmail.com>
2018-06-26 08:58:53 -0700

    Add doc comment describing complixity of merge

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -9,6 +9,35 @@ def mergesort(arr):
     return merge(L, R)
 
 def merge(a, b):
+    """
+    Merges two contending lists by picking either head
+    element of either list which is lesser than or equal
+    to the other and append it to the output list and remove the
+    element from the list. After the either list is empty,
+    it exhausts the leftover list and append all its element
+    to the outputting list.
+
+    Time complexity:
+
+    The first `while` loop will work with approximately `len(a) + len(b) / 2`
+    elements in average cases when `len(a)` and `len(b)` are roughly comparable.
+    That's O(a + b / 2) or O(N).
+
+    Appending to a list is an O(1) operation, so O(N + 1) stays O(N).
+
+    Removing an element anywhere other than the last position from a list
+    incur an O(N) work. This potentially makes this `while` loop a O(N ^ 2).
+
+    Either the second or third `while` loop will be called, and in average cases
+    the leftover list should contains around N / 2 elements so this will incur
+    O(N / 2) work for moving each element to the outputting list until empty, which
+    is roughly O(N).
+    Removing each element will require O(N) for all elements and thus becomes
+    O(N ^ 2).
+
+    The totally time complexity is 2 * O(N ^ 2) ~= O(N ^ 2).
+    """
+
     out = []
 
     while (len(a) > 0 and len(b) > 0):
